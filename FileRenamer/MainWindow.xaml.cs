8e0b5d76009fa89db461602d4ed280c64aa567fa
Basit Anwer <BAnwer@lmkr.com>
2020-08-20 17:22:30 +0500

    Init

---
+++ FileRenamer/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace FileRenamer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string folderName;
+        List<FileName> map = new List<FileName>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = folderName;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                FolderName.Content = Path.GetFileName(dialog.FileName);
+                folderName = dialog.FileName;
+                FillData();
+            }
+        }
+
+
+
+        private void FillData()
+        {
+            if (folderName == null || folderName.Length == 0)
+                return;
+
+            map.Clear();
+            map = new List<FileName>();
+            var sorted = Directory.GetFiles(folderName, Search.Text.Length == 0 ? "*" : Search.Text).OrderBy(f => f, new NaturalSorter());
+            int count = 0;
+            int.TryParse(NumberingFrom.Text,out count);
+            foreach (var item in sorted)
+            {
+                string pre = count <= 9 ? "0" : "";
+                map.Add(new FileName() 
+                { 
+                    OldName = item, 
+                    NewName = BaseName.Text + pre + count++ + Path.GetExtension(item) 
+                }); ;
+            }
+            Data.ItemsSource = map;
+
+        }
+
+        public class FileName
+        {
+            private string oldName;
+            private string newName;
+
+            public string OldName { get => Path.GetFileName(oldName); set => oldName = value; }
+            public string NewName { get => newName; set => newName = value; }
+            public string Fullpath { get => oldName;}
+        }
+
+        private void Search_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            FillData();
+        }
+
+        private void Rename_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in map)
+            {
+                System.IO.File.Move(item.Fullpath, Path.GetDirectoryName(item.Fullpath) + "\\" + item.NewName);
+            }
+            FillData();
+        }
+    }
+}
---
+++ FileRenamer/NaturalSorter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer
+{
+    public class NaturalSorter : IComparer<string>
+    {
+        private int ascii0 = (int)'0';
+        private int ascii9 = (int)'9';
+
+        public int Compare(string x, string y)
+        {
+            x = Path.GetFileNameWithoutExtension(x);
+            y = Path.GetFileNameWithoutExtension(y);
+            int len = x.Length > y.Length ? x.Length : y.Length;
+            int res = x.Length > y.Length ? 1 : x.Length == y.Length ? 0 : -1;
+
+            int xd = 0, yd = 0;
+
+            for (int i = 0, j = 0; i < len; i++)
+            {
+                if (i >= x.Length || j >= y.Length)
+                    return res;
+
+
+                if (IsNumber(x[i]) && IsNumber(y[j]))
+                {
+                    while (i < x.Length && IsNumber(x[i]))
+                    {
+                        xd *= 10;
+                        xd += GetNum(x[i]);
+                        i++;
+                    }
+                    while (j < y.Length && IsNumber(y[j]))
+                    {
+                        yd *= 10;
+                        yd += GetNum(y[j]);
+                        j++;
+                    }
+
+                    if (xd < yd)
+                        return -1;
+                    else if (xd > yd)
+                        return 1;
+
+                    j++;
+                    continue;
+                }
+
+                if (IsNumber(x[i]))
+                    return 1;
+                if (IsNumber(y[j]))
+                    return -1;
+
+                if (x[i] < y[j])
+                    return 1;
+                else if (x[i] > y[j])
+                    return -1;
+
+                j++;
+
+            }
+
+            return 0;
+        }
+
+        private bool IsNumber(char x)
+        {
+            if ((int)x >= ascii0 && (int)x <= ascii9)
+                return true;
+            return false;
+        }
+
+        private int GetNum(char x)
+        {
+            return (int)x - ascii0;
+        }
+    }
+}
